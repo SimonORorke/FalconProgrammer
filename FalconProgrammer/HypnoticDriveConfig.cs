1922b091ec89e90d57c66815ffffad84e9ac6ce8
Simon O'Rorke <simon.ororke@outlook.com>
2022-12-09 15:30:17 +1300

    Backed out changes to template access. #2

--- FalconProgrammer/HypnoticDriveConfig.cs
+++ FalconProgrammer/HypnoticDriveConfig.cs
@@ -2,9 +2,9 @@
 
 public class HypnoticDriveConfig : ScriptConfig {
   /// <summary>
-  ///   TODO: Look up template program path and script processor name in ScriptConfig.  
+  ///   TODO: Look up template program path and script processor name in ScriptConfig.
   /// </summary>
   public HypnoticDriveConfig() : base(
-    @"D:\Simon\OneDrive\Documents\Music\Software\UVI Falcon\Programs ORIGINAL\Hypnotic Drive\Leads TEMPLATE\Lead - Acid Gravel.uvip",
-    "EventProcessor99") { }
+    "Hypnotic Drive", "Leads",
+    "Acid Gravel", "EventProcessor99") { }
 }
