fc664425d883dac97ebb811457fbba9de29fc57f
Simon O'Rorke <simon.ororke@outlook.com>
2023-08-30 18:35:45 +1200

    Runner only

--- FalconProgrammer/Runner.cs
+++ FalconProgrammer/Runner.cs
@@ -8,13 +8,13 @@ public static class Runner {
   public static void Run() {
     var batch = new Batch();
     //batch.MoveConnectionsBeforeProperties("Devinity", "Bass", "Comber Bass");
-    batch.MoveConnectionsBeforeProperties(null);
+    // batch.MoveConnectionsBeforeProperties(null);
     // batch.RollForward("Devinity", "Bass", "Comber Bass");
     // batch.RollForward("Devinity", "Bass");
     // batch.RollForward("Devinity", "Plucks-Leads", "Pluck Sphere");
     // batch.RollForward("Devinity");
     // batch.RollForward("Factory", "Brutal Bass 2.1", "808 Line");
     // batch.RollForward("Devinity", "Plucks-Leads", "Harmony Plucks");
-    // batch.RollForward(null);
+    batch.RollForward(null);
   }
 }
