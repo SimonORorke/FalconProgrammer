ae3fc8b90c9516543713ffae4d0da9c46093918b
Simon O'Rorke <simon.ororke@outlook.com>
2023-12-24 16:45:12 +1300

    Old style Program class

--- FalconProgrammer/Program.cs
+++ FalconProgrammer/Program.cs
@@ -1,15 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics.CodeAnalysis;
 
-using FalconProgrammer;
+namespace FalconProgrammer;
 
-try {
-  Runner.Run();
-} catch (ApplicationException e) {
-  Console.Error.WriteLine("===================================");
-  Console.Error.WriteLine("Application Exception:");
-  Console.Error.WriteLine("===================================");
-  Console.Error.WriteLine(e.Message);
-  Console.Error.WriteLine("===================================");
-  Environment.Exit(1);
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+internal class Program {
+  private static void Main(string[] args) {
+    try {
+      Runner.Run();
+    } catch (ApplicationException e) {
+      Console.Error.WriteLine("===================================");
+      Console.Error.WriteLine("Application Exception:");
+      Console.Error.WriteLine("===================================");
+      Console.Error.WriteLine(e.Message);
+      Console.Error.WriteLine("===================================");
+      Environment.Exit(1);
+    }
+    Console.WriteLine("Finished");
+  }
 }
-Console.WriteLine("Finished");
