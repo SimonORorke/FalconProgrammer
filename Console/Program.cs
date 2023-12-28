416a41aa2833750f44df2d74df855c13c94c6999
Simon O'Rorke <simon.ororke@outlook.com>
2023-12-29 09:17:54 +1300

    FalconProgrammer.Console application

---
+++ Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using FalconProgrammer.Batch;
+using JetBrains.Annotations;
+
+namespace FalconProgrammer.Console;
+
+[PublicAPI] internal class Program {
+  private static void Main(string[] args) {
+    try {
+      if (args.Length > 0) {
+        new Batch.Batch().RunScript(args[0]);
+      } else {
+        Runner.Run();
+      }
+    } catch (ApplicationException e) {
+      System.Console.Error.WriteLine("===================================");
+      System.Console.Error.WriteLine("Application Exception:");
+      System.Console.Error.WriteLine("===================================");
+      System.Console.Error.WriteLine(e.Message);
+      System.Console.Error.WriteLine("===================================");
+      Environment.Exit(1);
+    }
+    System.Console.WriteLine("Finished");
+  }
+}
