b93fc7a63db0ad5e37a793f2faaf67e19d330e33
Simon O'Rorke <simon.ororke@outlook.com>
2024-03-21 08:52:54 +1300

    Comment only

--- Model/ISerialiser.cs
+++ Model/ISerialiser.cs
@@ -2,8 +2,9 @@
 
 /// <summary>
 ///   A utility that can serialise an object to a file.
-///   TODO: Make ISerialiser generic?
 /// </summary>
 public interface ISerialiser {
+  // There's not point in making Serialise generic, as the Type is only used to pass
+  // directly to XmlSerializer.
   void Serialise(Type type, object objectToSerialise, string outputPath);
 }
