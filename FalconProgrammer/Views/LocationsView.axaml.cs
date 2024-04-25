c2d399535447a07acf61a551e06eccf74358efa2
Simon O'Rorke <simon.ororke@outlook.com>
2024-04-25 17:01:44 +1200

    Settings Folder button has an access key.

--- FalconProgrammer/Views/LocationsView.axaml.cs
+++ FalconProgrammer/Views/LocationsView.axaml.cs
@@ -6,17 +6,9 @@ namespace FalconProgrammer.Views;
 
 public partial class LocationsView : UserControl {
   public LocationsView() {
-    InitializeComponent();
     // This only sets the DataContext for the previewer in the IDE.
     Design.SetDataContext(this,
       new LocationsViewModel(new DialogService(), new DispatcherService()));
-    // SettingsFolderButton.GotFocus += SettingsFolderButtonOnGotFocus;
+    InitializeComponent();
   }
-
-  // This proves that when the Settings Folder label's alt-key shortcut is pressed,
-  // the Browse button does get focus. The problem is that there's no focus rectangle.
-  // TODO: Investigate lack of focus rectangle on button when label's alt access key is pressed.
-  // private void SettingsFolderButtonOnGotFocus(object? sender, GotFocusEventArgs e) {
-  //   Debug.WriteLine("LocationsView.SettingsFolderButtonOnGotFocus");
-  // }
 }
