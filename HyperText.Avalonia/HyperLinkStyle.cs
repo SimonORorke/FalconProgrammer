45a9ea4fbea414d460dae0c27da2331deb384434
Simon O'Rorke <simon.ororke@outlook.com>
2024-05-15 11:49:10 +1200

    Fixed HyperLinkStyle warnings

--- HyperText.Avalonia/HyperLinkStyle.cs
+++ HyperText.Avalonia/HyperLinkStyle.cs
@@ -1,100 +1,84 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Markup.Xaml.Styling;
 using Avalonia.Styling;
-
+using HyperText.Avalonia.Controls;
 
 namespace HyperText.Avalonia;
 
-public class HyperLinkStyle : Styles, IResourceProvider, IStyle
-{
-    private readonly IStyle _controlsStyles;
-    private bool _isLoading;
-    private IStyle? _loaded;
+/// <summary>
+///   Based on https://github.com/AvaloniaUtils/HyperText.Avalonia.
+///   See <see cref="Hyperlink " /> summary.
+/// </summary>
+public class HyperLinkStyle : Styles, IResourceProvider, IStyle {
+  private readonly IStyle _controlsStyles;
+  private bool _isLoading;
+  private IStyle? _loaded;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="FluentTheme"/> class.
-    /// </summary>
-    /// <param name="baseUri">The base URL for the XAML context.</param>
-    public HyperLinkStyle(Uri? baseUri)
-    {
-        _controlsStyles = new StyleInclude(baseUri)
-        {
-            Source = new Uri("avares://HyperText.Avalonia/Styles/HyperlinkStyle.axaml")
-        };
+  /// <summary>
+  ///   Initializes a new instance of the <see cref="HyperLinkStyle" /> class.
+  /// </summary>
+  /// <param name="baseUri">The base URL for the XAML context.</param>
+  private HyperLinkStyle(Uri? baseUri) {
+    _controlsStyles = new StyleInclude(baseUri) {
+      Source = new Uri("avares://HyperText.Avalonia/Styles/HyperlinkStyle.axaml")
+    };
+  }
+
+  /// <summary>
+  ///   Initializes a new instance of the <see cref="HyperLinkStyle" /> class.
+  /// </summary>
+  /// <param name="serviceProvider">The XAML service provider.</param>
+  public HyperLinkStyle(IServiceProvider serviceProvider)
+    : this(((IUriContext)serviceProvider.GetService(typeof(IUriContext)))?.BaseUri) { }
+
+  /// <summary>
+  ///   Gets the loaded style.
+  /// </summary>
+  private IStyle Loaded {
+    get {
+      if (_loaded != null) {
+        return _loaded!;
+      }
+      _isLoading = true;
+      _loaded = new Styles { _controlsStyles };
+      _isLoading = false;
+      return _loaded!;
     }
+  }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="FluentTheme"/> class.
-    /// </summary>
-    /// <param name="serviceProvider">The XAML service provider.</param>
-    public HyperLinkStyle(IServiceProvider serviceProvider)
-        : this(((IUriContext)serviceProvider.GetService(typeof(IUriContext)))?.BaseUri)
-    {
+  public new IResourceHost? Owner => (Loaded as IResourceProvider)?.Owner;
+
+  public new bool TryGetResource(object key, ThemeVariant? theme, out object? value) {
+    if (!_isLoading && Loaded is IResourceProvider p) {
+      return p.TryGetResource(key, theme, out value);
     }
+    value = null;
+    return false;
+  }
 
+  bool IResourceNode.HasResources => (Loaded as IResourceProvider)?.HasResources ?? false;
 
-    public IResourceHost? Owner => (Loaded as IResourceProvider)?.Owner;
-
-    /// <summary>
-    /// Gets the loaded style.
-    /// </summary>
-    public IStyle Loaded
-    {
-        get
-        {
-            if (_loaded != null) return _loaded!;
-            _isLoading = true;
-
-            _loaded = new Styles() { _controlsStyles };
-
-            _isLoading = false;
-
-            return _loaded!;
-        }
+  public new event EventHandler? OwnerChanged {
+    add {
+      if (Loaded is IResourceProvider rp) {
+        rp.OwnerChanged += value;
+      }
     }
-
-    public bool TryGetResource(object key, ThemeVariant? theme, out object? value)
-    {
-        if (!_isLoading && Loaded is IResourceProvider p)
-        {
-            return p.TryGetResource(key,theme, out value);
-        }
-
-        value = null;
-        return false;
+    remove {
+      if (Loaded is IResourceProvider rp) {
+        rp.OwnerChanged -= value;
+      }
     }
-    
+  }
 
-    bool IResourceNode.HasResources => (Loaded as IResourceProvider)?.HasResources ?? false;
-    IReadOnlyList<IStyle> IStyle.Children => _loaded?.Children ?? Array.Empty<IStyle>();
+  void IResourceProvider.AddOwner(IResourceHost owner) {
+    (Loaded as IResourceProvider)?.AddOwner(owner);
+  }
 
-    public event EventHandler OwnerChanged
-    {
-        add
-        {
-            if (Loaded is IResourceProvider rp)
-            {
-                rp.OwnerChanged += value;
-            }
-        }
-        remove
-        {
-            if (Loaded is IResourceProvider rp)
-            {
-                rp.OwnerChanged -= value;
-            }
-        }
-    }
+  void IResourceProvider.RemoveOwner(IResourceHost owner) {
+    (Loaded as IResourceProvider)?.RemoveOwner(owner);
+  }
 
-
-
-
-
-    void IResourceProvider.AddOwner(IResourceHost owner) => (Loaded as IResourceProvider)?.AddOwner(owner);
-    void IResourceProvider.RemoveOwner(IResourceHost owner) => (Loaded as IResourceProvider)?.RemoveOwner(owner);
-
-    private CancellationTokenSource? _currentCancellationTokenSource;
-    private Task? _currentThemeUpdateTask;
+  IReadOnlyList<IStyle> IStyle.Children => _loaded?.Children ?? Array.Empty<IStyle>();
 }
