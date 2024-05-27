2d8db8500d837a3cb6926e0e8808cdbb3628e53e
Simon O'Rorke <simon.ororke@outlook.com>
2024-05-28 08:02:35 +1200

    Comments only

--- ViewModel/TabItemViewModel.cs
+++ ViewModel/TabItemViewModel.cs
@@ -1,11 +1,25 @@
 ﻿namespace FalconProgrammer.ViewModel;
 
+/// <summary>
+///   View model for a tab page, providing the tab header title and the view model for
+///   the page content.
+/// </summary>
+/// <remarks>
+///   Not to be renamed to TabItem, as we need to access the TabItem control in view code.
+/// </remarks>
 public class TabItemViewModel {
   public TabItemViewModel(ViewModelBase viewModel) {
     Header = viewModel.TabTitle;
     ViewModel = viewModel;
   }
 
+  /// <summary>
+  ///   Gets the tab header title.
+  /// </summary>
   public string Header { get; }
+  
+  /// <summary>
+  ///   Gets the view model for the page content. 
+  /// </summary>
   public ViewModelBase ViewModel { get; }
 }
