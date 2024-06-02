eb9d0d8cb89e249daf5e05d8f9c5f6976a2f6c61
Simon O'Rorke <simon.ororke@outlook.com>
2024-06-02 18:02:19 +1200

    Comment only.

--- ViewModel/IWindowLocationService.cs
+++ ViewModel/IWindowLocationService.cs
@@ -1,11 +1,25 @@
 ﻿namespace FalconProgrammer.ViewModel;
 
+/// <summary>
+///   Interface for a service that facilitates persistence of the main window's position,
+///   size and state.
+/// </summary>
 public interface IWindowLocationService {
   int? Left { get; set; }
   int? Top { get; set; }
   int? Width { get; set; }
   int? Height { get; set; }
   int? WindowState { get; set; }
+  
+  /// <summary>
+  ///  Uses previously saved data, if available, to size the window and position it on a
+  ///  screen. 
+  /// </summary>
   void Restore();
+  
+  /// <summary>
+  ///   Updates the data to be saved to settings from the window's current
+  ///   position/size/state.
+  /// </summary>
   void Update();
 }
