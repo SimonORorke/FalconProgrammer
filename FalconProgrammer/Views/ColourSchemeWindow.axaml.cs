0d198c3c9277b00bb1560e278c283313b7a5f218
Simon O'Rorke <simon.ororke@outlook.com>
2024-06-30 10:15:09 +1200

    Colour Scheme Window: Fixed ListBox item initial focus.

--- FalconProgrammer/Views/ColourSchemeWindow.axaml.cs
+++ FalconProgrammer/Views/ColourSchemeWindow.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using System.Linq;
+using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.VisualTree;
 using FalconProgrammer.Model;
@@ -15,11 +16,30 @@ public partial class ColourSchemeWindow : Window {
     var viewModel = (ColourSchemeWindowViewModel)DataContext!;
     viewModel.ChangeColourScheme += ViewModelOnChangeColourScheme;
     // Focusing the ListBox itself does not work. Focusing its selected item does.
-    // A weirdness with this is that up/down arrow navigation navigation through the
-    // ListBox items always starts with the top item.
     // (See comment in XAML for why we are using a ListBox instead of a ComboBox.)
-    var selectedItem = ColourSchemeListBox.FindDescendantOfType<ListBoxItem>()!;
-    selectedItem.Focus();
+    // Find the visual item to focus.
+    var listBoxItems = (
+      from descendant in ColourSchemeListBox.GetVisualDescendants()
+      where descendant is ListBoxItem
+      select (ListBoxItem)descendant).ToList();
+    // This fails to give ListBox items a focus rectangle.
+    // foreach (var listBoxItem in listBoxItems) {
+    //   listBoxItem.FocusAdorner = new FocusAdornerTemplate {
+    //     Content = new Border {
+    //       BorderBrush = new SolidColorBrush(Colors.White),
+    //       BorderThickness = new Thickness(2)
+    //     }
+    //   }!;
+    // }
+    var selectedListBoxItem = listBoxItems[ColourSchemeListBox.SelectedIndex];
+    selectedListBoxItem.Focus();
+    // We could give the focused item a focus rectangle, like this:
+    //   selectedListBoxItem.Focus(NavigationMethod.Directional);
+    // or this for a fatter rectangle:
+    //   selectedListBoxItem.Focus(NavigationMethod.Tab);
+    // But the focus rectangle will be lost as soon as another item is selected.
+    // The trick should be to instead define a FocusAdorner for the ListBoxItems.
+    // But I have not bee able to get that to work.
   }
 
   private static void ViewModelOnChangeColourScheme(object? sender, ColourSchemeId e) {
