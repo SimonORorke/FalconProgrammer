0c24274fc345a0432cca56eebb1e3e8c3a5ad34e
Simon O'Rorke <simon.ororke@outlook.com>
2024-06-30 14:08:54 +1200

    Give the focused ListBox item a focus rectangle

--- FalconProgrammer/Views/ColourSchemeWindow.axaml.cs
+++ FalconProgrammer/Views/ColourSchemeWindow.axaml.cs
@@ -1,7 +1,6 @@
-﻿using System.Linq;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.VisualTree;
 using FalconProgrammer.Model;
 using FalconProgrammer.ViewModel;
 
@@ -10,6 +9,21 @@ namespace FalconProgrammer.Views;
 public partial class ColourSchemeWindow : Window {
   public ColourSchemeWindow() {
     InitializeComponent();
+    ColourSchemeListBox.SelectionChanged += ColourSchemeListBoxOnSelectionChanged;
+  }
+
+  private void ColourSchemeListBoxOnSelectionChanged(
+    object? sender, SelectionChangedEventArgs e) {
+    if (IsVisible) {
+      FocusSelectedItem();
+    }
+  }
+
+  private void FocusSelectedItem() {
+    var selectedListBoxItem = (ListBoxItem)ColourSchemeListBox.ContainerFromIndex(
+      ColourSchemeListBox.SelectedIndex)!;
+    // NavigationMethod.Directional puts a focus rectangle round the selected item.
+    selectedListBoxItem.Focus(NavigationMethod.Directional);
   }
 
   protected override void OnLoaded(RoutedEventArgs e) {
@@ -17,21 +31,7 @@ public partial class ColourSchemeWindow : Window {
     viewModel.ChangeColourScheme += ViewModelOnChangeColourScheme;
     // Focusing the ListBox itself does not work. Focusing its selected item does.
     // (See comment in XAML for why we are using a ListBox instead of a ComboBox.)
-    // Find the visual item to focus.
-    var listBoxItems = (
-      from descendant in ColourSchemeListBox.GetVisualDescendants()
-      where descendant is ListBoxItem
-      select (ListBoxItem)descendant).ToList();
-    var selectedListBoxItem = listBoxItems[ColourSchemeListBox.SelectedIndex];
-    selectedListBoxItem.Focus();
-    // We could give the focused item a focus rectangle, like this:
-    //   selectedListBoxItem.Focus(NavigationMethod.Directional);
-    // or this for a fatter rectangle:
-    //   selectedListBoxItem.Focus(NavigationMethod.Tab);
-    // But the focus rectangle would be lost as soon as another item was selected.
-    // The trick should be to instead define a FocusAdorner for the ListBoxItems.
-    // But I have not bee able to get that to work. See commented out
-    // Style for ListBoxItem in App.xaml.
+    FocusSelectedItem();
   }
 
   private static void ViewModelOnChangeColourScheme(object? sender, ColourSchemeId e) {
