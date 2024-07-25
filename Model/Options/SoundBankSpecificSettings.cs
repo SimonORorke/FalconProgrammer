f3eb91e4083bbd9d600a60d08a5267a01a1cff9f
Simon O'Rorke <simon.ororke@outlook.com>
2024-07-25 17:16:00 +1200

    Refactor SoundBankSpecificSettings: Specify defaults in constructor.

--- Model/Options/SoundBankSpecificSettings.cs
+++ Model/Options/SoundBankSpecificSettings.cs
@@ -3,15 +3,28 @@
 namespace FalconProgrammer.Model.Options;
 
 public class SoundBankSpecificSettings {
-  [XmlElement] public StandardLayoutSetting EtherFields { get; set; } =
+  public SoundBankSpecificSettings() {
+    EtherFields.StandardLayout = true;
+    Fluidity.MoveAttackMacroToEnd = true;
+    OrganicPads.AttackSeconds = 0.02f;
+    OrganicPads.ReleaseSeconds = 0.3f;
+    OrganicPads.MaxAttackSeconds = 1f;
+    OrganicPads.MaxDecaySeconds = 15f;
+    OrganicPads.MaxReleaseSeconds = 2f;
+    Spectre.StandardLayout = true;
+  }
+
+  [XmlElement]
+  public StandardLayoutSetting EtherFields { get; set; } =
     new StandardLayoutSetting();
-    
+
   [XmlElement] public FluiditySettings Fluidity { get; set; } = new FluiditySettings();
 
   [XmlElement]
   public OrganicPadsSettings OrganicPads { get; set; } =
     new OrganicPadsSettings();
-    
-  [XmlElement] public StandardLayoutSetting Spectre { get; set; } =
+
+  [XmlElement]
+  public StandardLayoutSetting Spectre { get; set; } =
     new StandardLayoutSetting();
 }
